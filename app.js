200c2448030a57c7c8d7d583f14f675c81a23603
gyaneman <gyanexus7@gmail.com>
2015-09-26 15:09:28 +0900

    init commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function() {
+    console.log('disconnected');
+  });
+});
